03fe9cf484db952b3ce18d360aafc3bf0619478c
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2023-09-30 10:09:05 -0700

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1808,7 +1808,10 @@ async def auto_filter(client, msg, spoll=False):
             return
         if len(message.text) < 100:
             search = message.text
-            m=await message.reply_text(f"<b><i> 𝖲𝖾𝖺𝗋𝖼𝗁𝗂𝗇𝗀 𝖿𝗈𝗋 '{search}' 🔎</i></b>")
+            btn = [[
+                    InlineKeyboardButton(f"Searching  🔍  for {search}", url=CHNL_LNK)
+                    ]]
+            m = await message.reply_sticker('CAACAgIAAxkBAAEBUxhlF_3xP68-r1W66p5ckVPayyDhKQACcQgAAoSUQUlvaAkaprvOcx4E', reply_markup=InlineKeyboardMarkup(btn))
             search = search.lower()
             find = search.split(" ")
             search = ""
@@ -1837,7 +1840,10 @@ async def auto_filter(client, msg, spoll=False):
     else:
         message = msg.message.reply_to_message  # msg will be callback query
         search, files, offset, total_results = spoll
-        m=await message.reply_sticker_text(f"\n<b><i> 𝖲𝖾𝖺𝗋𝖼𝗁𝗂𝗇𝗀 𝖿𝗈𝗋 '{search}' 🔎</i></b>")
+        btn = [[
+                InlineKeyboardButton(f"Searching  🔍  for {search}", url=CHNL_LNK)
+                ]]
+        m = await message.reply_sticker('CAACAgIAAxkBAAEBUxhlF_3xP68-r1W66p5ckVPayyDhKQACcQgAAoSUQUlvaAkaprvOcx4E', reply_markup=InlineKeyboardMarkup(btn))
         settings = await get_settings(message.chat.id)
         await msg.message.delete()
     pre = 'filep' if settings['file_secure'] else 'file'
